c27f2a471f87b27d123c9d0f5a210928223ff759
Bessie Arino <brarino@gmail.com>
2018-01-30 09:59:48 -0800

    cropped header image, worked on hamburger menu

--- public/scripts/controller/routes.js
+++ public/scripts/controller/routes.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// page('/', window.landingView.init);
- page('/explorer/:rover', window.explorerView.init, window.roverData.fetchCameras);
-// page('/login', window.loginView.init);
-// page('/aboutus', window.aboutUsView.init);
-//page('/explorer/:rover/results', resultsView.init);
-// page('/createaccount', createAccountView.init);
-// page('/accountview/:user', accountView.init);
+page('/', window.landingView.init);
+page('/explorer/:rover', window.explorerView.init, window.roverData.fetchCameras);
+page('/login', window.loginView.init);
+page('/aboutus', window.aboutUsView.init);
+page('/explorer/:rover/results', resultsView.init);
+page('/createaccount', createAccountView.init);
+page('/accountview/:user', accountView.init);
 
 page();
