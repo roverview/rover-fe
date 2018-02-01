3a2a7055343e599326e9bf6920252324c8affb28
Jay Adams <j.adams011@hotmail.com>
2018-01-31 17:05:30 -0800

    change rover name in favorites to uppercase.  Still needs work.

--- public/scripts/model/app.js
+++ public/scripts/model/app.js
@@ -177,7 +177,7 @@
         data.rows.forEach(function(item) {
           let content = `
             <form>
-            <p><strong>Rover:</strong> ${item.rover_name}</p>
+            <p><strong>Rover:</strong> ${item.rover_name.toUpperCase()}</p>
             <p><strong>Earth Date:</strong> ${item.earth_date}</p>
             <p><strong>Camera:</strong> ${item.camera_name}</p>
             <img src="${item.image_src}">
